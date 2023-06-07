73e483a8ccfcda40863602e50389aaee2905451f
Ridwan Arifandi <orangerdigiart@gmail.com>
2023-06-07 11:52:24 +0700

    initialization

---
+++ includes/class-woofastcheck-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://ridwan-arifandi.com
+ * @since      1.0.0
+ *
+ * @package    Woofastcheck
+ * @subpackage Woofastcheck/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Woofastcheck
+ * @subpackage Woofastcheck/includes
+ * @author     Ridwan Arifandi <orangerdigiart@gmail.com>
+ */
+class Woofastcheck_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
